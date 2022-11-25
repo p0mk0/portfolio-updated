d2c2aabe09df004846df86f0903c60374eac6324
pomko <109567730+p0mk0@users.noreply.github.com>
2022-11-25 09:18:55 +0300

    Create AnnouncementsServer.lua

---
+++ announcements/AnnouncementsServer.lua
@@ -0,0 +1,10 @@
+local Request, Execute, mainID, minRank = game.ReplicatedStorage.RequestAnnouncement, game.ReplicatedStorage.ExecuteAnnouncement, 0, 0
+
+-- \ This code wouldn't actually work if you just put it in as the mainID and minRank are blanked out
+-- \ Acts as a redirect for announcement requests, also verifies requests
+
+Request.OnServerEvent:Connect(function(plr,header,content,duration)
+	if plr:GetRankInGroup(mainID) >= minRank then
+		Execute:FireAllClients(header,content,duration)
+	end
+end)
